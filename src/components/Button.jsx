13c5c3aab016845604112eab33f13ae5517b254e
Sagar Subedi <sagar@example.com>
2022-08-18 13:04:57 -0500

    refactor: removed react import statement

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 const Button = ({styles, text}) => (
   <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles}`}>{text}</button>
