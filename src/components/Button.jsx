404ebb28ada59d8437033f5335a8a553c546c133
Sagar Subedi <sagar@example.com>
2022-08-17 20:28:40 -0500

    feat: generic button component completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({styles, text}) => (
+  <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles}`}>{text}</button>
+)
 
 export default Button
